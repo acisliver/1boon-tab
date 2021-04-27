c33b4f4b4d8e2e7e8a865201aff0d2f64992721c
acisliver <7273322@naver.com>
2021-04-28 00:17:17 +0900

    popular.json의 title값들을 id=list에 표시

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import activateTab from "/modules/navTab.js"
+import showContent from "./modules/showContent.js"
 
 //각 탭에 이벤트리스너 등록
 const $tabs = document.querySelectorAll(".nav li")
@@ -7,4 +8,6 @@ $tabs.forEach(tab => {
     tab.addEventListener('click', ()=>activateTab($tabs, tab))
 })
 
+const $list = document.getElementById("list")
+showContent("popular", $list)
 
--- modules/showContent.js
+++ modules/showContent.js
@@ -1,5 +1,5 @@
 
-export default function (content) {
+export default function (content, list) {
 
     //로컬에 api 요청을 통해 json파일 load
     fetch(`http://localhost:5000/${content}.json`)
@@ -9,7 +9,13 @@ export default function (content) {
     function jsonToHtml(json) {
         console.log(json)
 
-        
+        for (let i = 0; i < json.length; i++){
+            const title = json[i].title
+            const testNode = document.createTextNode(title)
+            const titleDiv = document.createElement("div")
+            titleDiv.appendChild(testNode)
+            list.appendChild(titleDiv)
+        }
     }
 
 

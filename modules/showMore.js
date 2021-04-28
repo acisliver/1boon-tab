64ab3ff8a43f8bc2f510cbeda2e7b6cfe765779c
acisliver <7273322@naver.com>
2021-04-28 16:03:41 +0900

    showMore 수정

--- modules/showMore.js
+++ modules/showMore.js
@@ -7,12 +7,14 @@ import getJson from "./getJson.js";
 export default async function (content, list) {
     //json을 불러옴
     const json = await getJson(content)
+    //현재 표시된 게시물의 개수
+    const currentArticleNum = list.childElementCount
 
     //더보기의 하위 노드개수와 json의 개수를 비교해 더보기가 이미 되었는지 확인
-    if(list.childElementCount === json.length) return
+    if(currentArticleNum === json.length) return
 
-    //json을 html로
-    for (let i = 10; i < json.length; i++) {
+    //10개씩 더 표시
+    for (let i = currentArticleNum; i < currentArticleNum + 10; i++) {
         jsonToHtml(i, json);
     }
 }

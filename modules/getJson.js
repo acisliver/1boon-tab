161e4bdae43b9fb985c109ec949975933e1d3dd7
acisliver <7273322@naver.com>
2021-06-24 15:17:16 +0900

    배포를 위한 수정
    serve 시 로컬호스트로 fetch를 했으나 개발환경에서 배포환경으로 바꿀 시 제대로 fetch하지 못하여 로컬호스트 부분 삭제

--- modules/getJson.js
+++ modules/getJson.js
@@ -2,7 +2,7 @@
 export default async function (content) {
 
     //로컬에 api 요청을 통해 json파일 load
-    const response = await fetch(`http://localhost:5000/${content}.json`)
+    const response = await fetch(`${content}.json`)
 
     //Promise를 json으로
     const data = await response.json()
